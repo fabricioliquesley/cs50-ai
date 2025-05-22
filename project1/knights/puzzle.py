18dd5270771a28f8dd57396650100ee8b6c62e4d
Fabricio Liqueley <fabricioliquesley15@gmail.com>
2025-05-22 10:59:58 -0300

    solved the first challenge of project1

--- project1/knights/puzzle.py
+++ project1/knights/puzzle.py
@@ -12,21 +12,27 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Implication(Not(AKnight), AKnave),
+    Not(AKnight)
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    And(Or(AKnave, AKnight), Or(BKnave, BKnight)),
+    Biconditional(BKnight, Not(AKnight)),
+    Not(AKnight)
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    And(Or(AKnave, AKnight), Or(BKnave, BKnight)),
+    Biconditional(BKnight, AKnave),
+    Not(BKnave)
 )
 
 # Puzzle 3
@@ -35,7 +41,10 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    And(Or(AKnave, AKnight), Or(BKnave, BKnight), Or(CKnave, CKnight)),
+    Biconditional(BKnave, CKnight),
+    Implication(CKnight, AKnight),
+    Not(BKnight)
 )
 
 
